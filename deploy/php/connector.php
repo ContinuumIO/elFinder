b9f5f785a9cecc64dc13e982cb0dcf516a26d89f
Karissa McKelvey <krmckelv@gmail.com>
2013-10-16 17:20:24 -0500

    FIX: prepare elfinder for deployment on ami

--- deploy/php/connector.php
+++ deploy/php/connector.php
@@ -34,7 +34,6 @@ $opts = array(
         array(
             'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
             'path'          =>  getenv("HOME"),  // path to files (REQUIRED)
-            'URL'           => 'http://localhost/files/home/',   // URL to files (REQUIRED)
             'alias'         => 'Home', // The name to replace your actual path name. (OPTIONAL)
             'accessControl' => 'access', // disable and hide dot starting files (OPTIONAL)
             'tmbURL'        => 'https://d3uybqv7a64u59.cloudfront.net/latest/apps/elFinder/img' 
@@ -42,7 +41,6 @@ $opts = array(
         array(
             'driver'        => 'LocalFileSystem',
             'path'          => getenv("PROJECT_HOME"),
-            'URL'           => 'http://localhost/files/project/',
             'alias'         => getenv("PROJECT_NAME"),
             'accessControl' => 'access',      // disable and hide dot starting files (OPTIONAL)
             'tmbURL'        => 'https://d3uybqv7a64u59.cloudfront.net/latest/apps/elFinder/img' 
--- elFinder.sh
+++ elFinder.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 PORT=$1
+URLPREFIX=$2
 el_dir=/opt/wakari/elFinder/
-cd $el_dir/deploy
-/usr/bin/php -S 0.0.0.0:$1
+user_project_dir=${el_dir}/deploy${URLPREFIX}
+
+echo ${user_project_dir}
+if [ ! -d ${user_project_dir} ] 
+    then
+        echo "Creating dir"
+        mkdir -p ${user_project_dir}
+        cp ${el_dir}/deploy/index.html ${user_project_dir}/index.html
+        cp -r ${el_dir}/deploy/php ${user_project_dir}/php
+fi
+cd ${el_dir}/deploy
+/usr/local/php5/bin/php -S 0.0.0.0:$PORT 
